fb9a751e688e33cdd1d457e1192aa02d347e1461
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2017-03-26 10:16:30 +0200

    Refactoring...

--- src/board/events/index.js
+++
@@ -1,8 +0,0 @@
-/** @type {String} */ export const COMMAND        = 'command'
-/** @type {String} */ export const COMMAND_CREATE = COMMAND + '/create'
-/** @type {String} */ export const COMMAND_SEND   = COMMAND + '/send'
-/** @type {String} */ export const COMMAND_SENT   = COMMAND + '/sent'
-/** @type {String} */ export const COMMAND_RETRY  = COMMAND + '/retry'
-/** @type {String} */ export const COMMAND_ERROR  = COMMAND + '/error'
-
-/** @type {String} */ export const RESPONSE = 'response'
--- src/board/index.js
+++
@@ -1,5 +0,0 @@
-import Board from './board'
-import * as events from './events'
-import * as commands from './commands'
-export default Board
-export { Board, events, commands }
