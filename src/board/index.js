659844ead55bdb8274436d010c59b3e1959b1400
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2017-04-08 10:20:32 +0200

    Exports boardTopics and boardCommands

--- src/board/index.js
+++ src/board/index.js
@@ -1,4 +1,6 @@
 import Board from './board'
+import boardTopics from './topics'
+import * as boardCommands from './commands'
 
 export default Board
-export { Board }
+export { Board, boardTopics, boardCommands }
