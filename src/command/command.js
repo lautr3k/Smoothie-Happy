3815eb05130781a01f0a046750902f97d8286a65
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2019-07-08 09:19:36 +0200

    move parallel commands check to request

--- src/command/command.js
+++ src/command/command.js
@@ -1,21 +1,7 @@
-import { PARALLEL_COMMANDS, UNSUPPORTED_COMMAND } from './error-types'
+import { UNSUPPORTED_COMMAND } from './error-types'
 import { errorFactory } from '../request/factory'
+import { requiredParam, requiredTypes } from '../utils'
 import post from '../request/post'
-
-/**
- * True if a command is waiting for a response.
- * @type {Object}
- */
-let sent = {}
-
-/**
- * Test if a command is waiting for a response.
- * @param  {String} address
- * @return {Boolean}
- */
-export function isSent (address) {
-  return !!sent[address]
-}
 
 /**
  * Send arbitrary command.
@@ -34,41 +20,26 @@ export function isSent (address) {
  * [EXAMPLE ../../examples/command.js]
  */
 export default function command ({
-  address,
-  command,
+  address = requiredParam('address'),
+  command = requiredParam('command'),
   ...rest
 } = {}) {
-  return new Promise(function (resolve, reject) {
-    const params = {
-      ...rest,
-      command,
-      url: `http://${address}/command`,
-      data: `${command}\n`
-    }
-    if (isSent(address)) {
-      reject(errorFactory({
-        type: PARALLEL_COMMANDS,
-        message: 'Parallel commands prohibited. Please wait for the end of a command before sending another',
-        params,
-        xhr: null
-      }))
-      return
-    }
-    sent[address] = true
-    post(params).then(response => {
-      sent[address] = false
-      if (response.text.startsWith('error:Unsupported command')) {
-        throw errorFactory({
-          ...response,
-          type: UNSUPPORTED_COMMAND,
-          message: `Unsupported command [ ${command} ]`
-        })
-      }
-      resolve(response)
-    })
-      .catch(error => {
-        sent[address] = false
-        reject(error)
+  requiredTypes('address', address, ['string'])
+  requiredTypes('command', command, ['string'])
+  const params = {
+    ...rest,
+    command,
+    url: `http://${address}/command`,
+    data: `${command}\n`
+  }
+  return post(params).then(response => {
+    if (response.text.startsWith('error:Unsupported command')) {
+      throw errorFactory({
+        ...response,
+        type: UNSUPPORTED_COMMAND,
+        message: `Unsupported command [ ${command} ]`
       })
+    }
+    return response
   })
 }
